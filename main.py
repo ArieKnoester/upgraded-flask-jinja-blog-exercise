d9e22cb2a811149d60584437fd7e265598b04ceb
Arie Knoester <arikstr@gmail.com>
2024-01-29 11:37:06 -0600

    Added contact form functionality.

--- main.py
+++ main.py
@@ -7,9 +7,20 @@
 # Newly created 'APIs' go away after a while. A new one may need
 # to be created if the request returns null. Data for these fake
 # posts can be found in the static/assets/data directory.
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import datetime
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+HOST_EMAIL = os.environ["HOST_EMAIL"]
+FROM_ADDR = os.environ["FROM_ADDR"]
+FROM_ADDR_APP_PASSWORD = os.environ["FROM_ADDR_APP_PASSWORD"]
+TO_ADDR = os.environ["TO_ADDR"]
 
 
 def request_dummy_blog_posts():
@@ -34,9 +45,48 @@ def about():
     return render_template("about.html", year=current_year)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html", year=current_year)
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message_body = request.form['message_body']
+        print(f"name: {name}\nemail: {email}\nphone: {phone}\nmessage: {message_body}")
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "New contact message from blog"
+        message["From"] = FROM_ADDR
+        message["To"] = TO_ADDR
+        text = f"name: {name}\nemail: {email}\nphone: {phone}\nmessage: {message_body}"
+        html = f"""
+        <html>
+          <head></head>
+          <body>
+            <p>
+                {name}<br>
+                {email}<br>
+                {phone}<br>
+                {message_body}<br>
+            </p>
+          </body>
+        </html>
+        """
+
+        part1 = MIMEText(text, 'plain')
+        part2 = MIMEText(html, 'html')
+        message.attach(part1)
+        message.attach(part2)
+
+        with smtplib.SMTP(HOST_EMAIL, port=587) as connection:
+            connection.starttls()
+            connection.login(user=FROM_ADDR, password=FROM_ADDR_APP_PASSWORD)
+            connection.sendmail(
+                from_addr=FROM_ADDR,
+                to_addrs=TO_ADDR,
+                msg=message.as_string()
+            )
+        return render_template("contact.html", year=current_year, msg_sent=True)
+    return render_template("contact.html", year=current_year, msg_sent=False)
 
 
 @app.route("/post/<int:post_id>")
