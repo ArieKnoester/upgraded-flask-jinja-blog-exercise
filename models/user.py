91bb70015255c907a1aadc2e31976f06299b039b
Arie Knoester <arikstr@gmail.com>
2024-02-19 11:35:10 -0600

    Added User model.

---
+++ models/user.py
@@ -0,0 +1,11 @@
+from models.db import db
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(100))
